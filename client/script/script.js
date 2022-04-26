9050a0f56f3364fdd263e51c0f2373e5abe8d520
mk <kosminskimateusz@gmail.com>
2022-04-26 15:37:07 +0200

    Creating search bar

--- client/script/script.js
+++ client/script/script.js
@@ -48,21 +48,99 @@ DIETS_BUTTON.addEventListener('click', toggleList);
 
 
 
-    // await fetch('http://localhost:5000/products/')
-    //     .then(res => res.json())
-    //     .then(data => products = data)
-    //     .then(() => console.log(products));
+// await fetch('http://localhost:5000/products/')
+//     .then(res => res.json())
+//     .then(data => products = data)
+//     .then(() => console.log(products));
+
+
+// async function getProducts() {
+//     try {
+//         let exam;
+//         const response = await (await fetch('https://www.boredapi.com/api/activity')
+//             .then(res => res.json())
+//             .then(data => exam = data));
+
+//         return exam;
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
+
+
+// FILL PRODUCTS TABLE WITH DATA
+
+const productsTable = document.querySelector("#products");
+const productsTBody = document.createElement("tbody");
+productsTBody.classList.add("products__body");
+
+console.log(window.location.href);
+console.log(window.location.hash);
+const clientUrl = 'http://127.0.0.1:5501/client';
+if (window.location.href === `${clientUrl}/products.html`) {
+    fetch(`${clientUrl}/sampleData/products.json`)
+        .then(response => response.json())
+        .then(data => {
+            for (const el of data.data) {
+                console.log(el.id);
+                const tableRow = document.createElement("tr");
+                // Add ID to table
+                const idTableData = document.createElement("td");
+                idTableData.innerHTML = el.id;
+                tableRow.appendChild(idTableData);
+                // Add Name to table
+                const nameTableData = document.createElement("td");
+                nameTableData.innerHTML = el.name;
+                tableRow.appendChild(nameTableData);
+                // Add Kcal to table
+                const kcalTableData = document.createElement("td");
+                kcalTableData.innerHTML = el.kcal;
+                tableRow.appendChild(kcalTableData);
+
+                productsTBody.appendChild(tableRow);
+            }
+            productsTable.appendChild(productsTBody);
+
+        })
+        .catch(error => console.log(error));
+} else if(window.location.href.includes("search__input")) {
+}
+
+const search = () => {
+    const searchInputValue = document.querySelector("#search__input").value;
+    console.log(searchInputValue);
+    fetch(`${clientUrl}/sampleData/products.json`)
+        .then(response => response.json())
+        .then(data => {
+            for (const el of data.data) {
+                if(el.name.includes(searchInputValue))
+                {
+                    console.log(el.id);
+                    const tableRow = document.createElement("tr");
+                    // Add ID to table
+                    const idTableData = document.createElement("td");
+                    idTableData.innerHTML = el.id;
+                    tableRow.appendChild(idTableData);
+                    // Add Name to table
+                    const nameTableData = document.createElement("td");
+                    nameTableData.innerHTML = el.name;
+                    tableRow.appendChild(nameTableData);
+                    // Add Kcal to table
+                    const kcalTableData = document.createElement("td");
+                    kcalTableData.innerHTML = el.kcal;
+                    tableRow.appendChild(kcalTableData);
     
-    
-    // async function getProducts() {
-    //     try {
-    //         let exam;
-    //         const response = await (await fetch('https://www.boredapi.com/api/activity')
-    //             .then(res => res.json())
-    //             .then(data => exam = data));
-    
-    //         return exam;
-    //     } catch (error) {
-    //         console.error(error.message);
-    //     }
-    // }
+                    productsTBody.appendChild(tableRow);
+
+                }
+            }
+            productsTable.appendChild(productsTBody);
+
+        })
+        .catch(error => console.log(error));
+
+}
+
+const searchButton = document.querySelector("#search__button");
+
+searchButton.addEventListener("click", search);
